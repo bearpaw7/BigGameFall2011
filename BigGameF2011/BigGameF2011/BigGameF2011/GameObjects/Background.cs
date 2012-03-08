3876b801d19a994a3b16fd8c5730dcc8833fd0f7
Chris Konkol <cj.konkol@gmail.com>
2012-03-08 03:54:40 +0000

    Adding clouds to the background

--- BigGameF2011/BigGameF2011/BigGameF2011/GameObjects/Background.cs
+++ BigGameF2011/BigGameF2011/BigGameF2011/GameObjects/Background.cs
@@ -16,13 +16,14 @@ namespace BigGameF2011.GameObjects
     {
         float ScrollVel;
 
-        Vector2 Pos1, Pos2, Pos3;
-        Texture2D Texture1, Texture2, Texture3;
+        Vector2 Pos1, Pos2, Pos3, Cloud_Pos1, Cloud_Pos2, Cloud_Pos3;
+        Texture2D Texture1, Texture2, Texture3, Cloud1, Cloud2, Cloud3;
 
         public Background(Vector2 pos1) : base(pos1)
         {
             ScrollVel = (float)3;
             Pos1 = pos1;
+            Cloud_Pos1 = pos1;
         }
 
         public override void Load(ContentManager Content)
@@ -31,11 +32,21 @@ namespace BigGameF2011.GameObjects
             Texture2 = Content.Load<Texture2D>("Backgrounds/Background2");
             Texture3 = Content.Load<Texture2D>("Backgrounds/Background3");
 
+            Cloud1 = Content.Load<Texture2D>("Backgrounds/Clouds");
+            Cloud2 = Content.Load<Texture2D>("Backgrounds/Clouds");
+            Cloud3 = Content.Load<Texture2D>("Backgrounds/Clouds");
+
             Pos2.X = Pos1.X;
             Pos3.X = Pos1.X;
 
+            Cloud_Pos2.X = Cloud_Pos1.X;
+            Cloud_Pos3.X = Cloud_Pos1.X;
+
             Pos2.Y = Pos1.Y - Texture1.Height;
             Pos3.Y = Pos2.Y - Texture2.Height;
+
+            Cloud_Pos2.Y = Cloud_Pos1.Y - Cloud1.Height;
+            Cloud_Pos3.Y = Cloud_Pos2.Y - Cloud2.Height;
 
         }
 
@@ -45,6 +56,10 @@ namespace BigGameF2011.GameObjects
             Shmup.spriteBatch.Draw(Texture1, Pos1, Color.White);
             Shmup.spriteBatch.Draw(Texture2, Pos2, Color.White);
             Shmup.spriteBatch.Draw(Texture3, Pos3, Color.White);
+            Shmup.spriteBatch.Draw(Cloud1, Cloud_Pos1, Color.White);
+            Shmup.spriteBatch.Draw(Cloud2, Cloud_Pos2, Color.White);
+            Shmup.spriteBatch.Draw(Cloud3, Cloud_Pos3, Color.White);
+
             base.Draw(gameTime);
         }
 
@@ -54,6 +69,10 @@ namespace BigGameF2011.GameObjects
             Pos1.Y += ScrollVel;
             Pos2.Y += ScrollVel;
             Pos3.Y += ScrollVel;
+
+            Cloud_Pos1.Y += (ScrollVel * 2);
+            Cloud_Pos2.Y += (ScrollVel * 2);
+            Cloud_Pos3.Y += (ScrollVel * 2);
 
             if (Pos1.Y > Texture1.Height)
             {
@@ -67,6 +86,20 @@ namespace BigGameF2011.GameObjects
             {
                 Pos3.Y = Pos2.Y - Texture2.Height;
             }
+
+            if (Cloud_Pos1.Y > Cloud1.Height)
+            {
+                Cloud_Pos1.Y = Cloud_Pos3.Y - Cloud3.Height;
+            }
+            if (Cloud_Pos2.Y > Cloud2.Height)
+            {
+                Cloud_Pos2.Y = Cloud_Pos1.Y - Cloud1.Height;
+            }
+            if (Cloud_Pos3.Y > Cloud3.Height)
+            {
+                Cloud_Pos3.Y = Cloud_Pos2.Y - Cloud2.Height;
+            }
+
             base.Update();
         }
     }
